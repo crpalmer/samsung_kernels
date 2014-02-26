8efa5c099cd276b7cd3ecf6ec2dd63c238416419
Christopher R. Palmer <crpalmer@gmail.com>
2014-02-26 06:39:49 -0500

    mondrianwifi: Cleanup files samsung accidentally released

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Jan 24 09:36:09 KST 2014"
-#define LINUX_COMPILE_BY "omsgsm"
-#define LINUX_COMPILE_HOST "sy918-desktop"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
