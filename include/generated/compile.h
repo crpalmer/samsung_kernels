6185ae2d40cb5fc09693893bdeec8803e6a4e24d
Christopher R. Palmer <crpalmer@gmail.com>
2014-02-26 06:39:49 -0500

    mondrianwifi: Cleanup files samsung accidentally released
    Conflicts:
    	Module.symvers
    	include/generated/autoconf.h
    	include/generated/compile.h

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Feb 11 14:52:31 KST 2014"
-#define LINUX_COMPILE_BY "usacdma"
-#define LINUX_COMPILE_HOST "shinalee-desktop"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
