d891b438c0fea069f673d2aaac146cde1068ff88
Christopher R. Palmer <crpalmer@gmail.com>
2014-11-11 06:35:28 -0500

    SM-T900_KK_Opensource.zip: T900XXUANB5

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Feb 6 18:24:14 KST 2014"
+#define LINUX_COMPILE_BY "shina.lee"
+#define LINUX_COMPILE_HOST "Sprint10"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
